e3b1b547cfd58141efdb3d93305f5321e31b20f1
Pedro Fernandes <a21908084@alunos.ulht.pt>
2021-03-29 19:11:59 +0100

    update scripts
    updated movefoward and shooting scripts

---
+++ Indeana Jones and the Lost Ark/Assets/Scripts/Shooting.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public bool             Shoot = false;
+    public int              Bullets = 5;
+    public GameObject       Bullet;
+    public GameObject       ShootingPoint;
+    public bool             player = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //temp
+        if (Input.GetButtonDown("Fire1") && player)
+        {
+            Shoot = true;
+        }
+        //temp
+
+        if (Shoot && Bullets > 0) 
+        {
+            Instantiate(Bullet, ShootingPoint.transform.position, ShootingPoint.transform.rotation);
+            Shoot = false;
+            Bullets -= 1;
+        }else if (Bullets <= 0) 
+        {
+            Shoot = false;
+        }
+    }
+}
