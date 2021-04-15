554e5c9b2a1a7c8ee6676740daeaaba230d23207
Pedro Fernandes <a21908084@alunos.ulht.pt>
2021-04-15 11:48:46 +0100

    update
    created the whip script, damage script and health scrip

--- Indeana Jones and the Lost Ark/Assets/Scripts/DamageZones.cs
+++ Indeana Jones and the Lost Ark/Assets/Scripts/DamageZones.cs
@@ -4,23 +4,14 @@ using UnityEngine;
 
 public class DamageZones : MonoBehaviour
 {
-    public bool Continues = false;
-    public bool DestroyOnContact = true;
-    public int Damage = 0;
-    public string DamagingTag;
-    public string IgnoreTag;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public bool         Continues = false;
+    public bool         DestroyOnContact = true;
+    public bool         DisableOnContact = false;
+    public int          Damage = 0;
+    public string       DamagingTag;
+    public string       IgnoreTag;
+    public string       IgnoreTag2;
+    public string   IgnoreTag3;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -28,9 +19,14 @@ public class DamageZones : MonoBehaviour
         {
             collision.GetComponent<Health>().DanmageHeal(-Damage);
         }
-        if (DestroyOnContact && collision.tag != IgnoreTag) 
+
+        if (DestroyOnContact && collision.tag != IgnoreTag && collision.tag != IgnoreTag2 && collision.tag != IgnoreTag3) 
         {
             Destroy(gameObject);
+        }
+        else if (DisableOnContact && collision.tag != IgnoreTag && collision.tag != IgnoreTag2 && collision.tag != IgnoreTag3)
+        {
+            gameObject.SetActive(false);
         }
     }
 }
